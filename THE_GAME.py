007aa7be37ca8fff123278f6cf5ac1b472ae2424
TEJAS <95134165+TejasManjunath@users.noreply.github.com>
2024-05-01 09:57:57 +0000

    upoading the game

--- THE_GAME.py
+++ THE_GAME.py
@@ -4,7 +4,6 @@ print("PLay this game at your own risk")
 
 while True:
     number = random.randint(1,1000000000000)
-    print(number)
     guess = int(input("GUess the number between 1 and 1000: "))
         
     if guess == number:
